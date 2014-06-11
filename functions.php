4cddd29e132fb48508c0b4ec9a0737654366ac6b
Eduardo Zulian <eduardozulian@gmail.com>
2014-06-11 09:44:48 -0300

    Change heading size for widget titles

--- functions.php
+++ functions.php
@@ -82,8 +82,8 @@ function ativista_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'ativista_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -14,14 +14,14 @@
 				</aside>
 
 				<aside id="archives" class="widget">
-					<h1 class="widget-title"><?php _e( 'Archives', 'ativista' ); ?></h1>
+					<h3 class="widget-title"><?php _e( 'Archives', 'ativista' ); ?></h3>
 					<ul>
 						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 					</ul>
 				</aside>
 
 				<aside id="meta" class="widget">
-					<h1 class="widget-title"><?php _e( 'Meta', 'ativista' ); ?></h1>
+					<h3 class="widget-title"><?php _e( 'Meta', 'ativista' ); ?></h3>
 					<ul>
 						<?php wp_register(); ?>
 						<li><?php wp_loginout(); ?></li>
