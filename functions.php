651fa95eefbd8bd1c286f0e617bbf494353c5f35
Eduardo Zulian <eduardozulian@gmail.com>
2014-07-06 08:13:07 -0300

    Change app ID

--- functions.php
+++ functions.php
@@ -158,7 +158,7 @@ function ativista_fb_scripts() {
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
 	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&appId=181431738599219&version=v2.0";
+	  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&appId=264414477093872&version=v2.0";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
 	<?php
