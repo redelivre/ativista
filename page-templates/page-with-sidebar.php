713e4c6c3871199b644e89aaba35f01410dcdc7c
Eduardo Zulian <eduardozulian@gmail.com>
2014-07-18 15:27:46 -0300

    Add FB comments to the page template

--- page-templates/page-with-sidebar.php
+++ page-templates/page-with-sidebar.php
@@ -17,12 +17,7 @@ get_header(); ?>
 
 					<?php get_template_part( 'content', 'page' ); ?>
 
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || '0' != get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
+					<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-numposts="5" data-colorscheme="light" data-width="100%"></div>
 
 				<?php endwhile; // end of the loop. ?>
 
