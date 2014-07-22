84029c18b3aa45202407e69d8a2882b7fc2387ef
Eduardo Zulian <eduardozulian@gmail.com>
2014-07-22 21:01:32 +0700

    Add Modernizr library

--- functions.php
+++ functions.php
@@ -126,6 +126,9 @@ function ativista_scripts() {
 	// Main stylesheet
 	wp_enqueue_style( 'ativista-style', get_stylesheet_uri() );
 
+	// Modernizer Custom Build (only the basics)
+	wp_enqueue_script( 'ativista-modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array( 'jquery' ), '2.8.3' );
+
 	wp_enqueue_script( 'ativista-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'ativista-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
